1aae40eeefb65ba06d776321b1e838905ff5ea4d
ayarzuki <aulya.yarzuki@gmail.com>
2022-04-22 01:25:26 +0700

    In the 29. Introduction to packages

--- main.go
+++ main.go
@@ -7,9 +7,19 @@ func main() {
 	foo()
 	fmt.Println("SOMETHING MORE")
 
-	
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
 }
 
 func foo() {
 	fmt.Println("FOO.")
+}
+
+func bar() {
+	fmt.Println("BAR.")
 }
---
+++ section4/intro_to_packages.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n, _ := fmt.Println("Hello, world.", 42, true)
+	fmt.Println(n)
+}
