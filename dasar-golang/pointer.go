91e87d58eba52b809d8e50a6b57e8fab5f97d86b
ayarzuki <aulya.yarzuki@gmail.com>
2022-07-13 01:27:47 +0700

    learn pointer golang

---
+++ dasar-golang/pointer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	// address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
+	// address2 := &address1
+	var address1 Address = Address{"Subang", "Jawa Barat", "Indonesia"}
+	var address2 *Address = &address1
+
+	address2.City = "Bandung"
+
+	address2 = &Address{"Jakarta", "Jawa Barat", "Indonesia"}
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+}
