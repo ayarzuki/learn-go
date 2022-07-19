5e3946037e5915e2e798324845d067a9ad89efac
ayarzuki <aulya.yarzuki@gmail.com>
2022-07-20 03:47:50 +0700

    learn import, package, and access modifier in golang

--- dasar-golang/helper/sayhello.go
+++ dasar-golang/helper/sayhello.go
@@ -2,6 +2,13 @@ package helper
 
 import "fmt"
 
+var version = 1.0
+var Application = "Belajar Golang"
+
 func SayHello(name string){
 	fmt.Println("Hello", name)
+}
+
+func sayGoodbye(name string){
+	fmt.Println("Goodbye", name)
 }
--- dasar-golang/import.go
+++ dasar-golang/import.go
@@ -1,7 +1,13 @@
 package main
 
-import "learn-go/dasar-golang/helper"
+import (
+	"fmt"
+	"learn-go/dasar-golang/helper"
+)
 
 func main() {
 	helper.SayHello("Rani")
+	// helper.sayGoodbye("Nisa") //error
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) //errpr
 }
